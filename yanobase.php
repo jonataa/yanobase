da1619f9d906eee4086f78043f06d2481d1b01d0
Jonata Weber <jonataweber@gmail.com>
2014-08-28 20:54:17 -0300

    Improvements in source code

---
+++ yanobase.php
@@ -0,0 +1,11 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use \Yanobase\Console\Command\MigrateCommand;
+use \Symfony\Component\Console\Application;
+
+$application = new Application('Yanobase', '0.1-alpha');
+$application->add(new MigrateCommand);
+$application->run();
